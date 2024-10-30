f6447988cacf7b903119e544f15a301883a0954b
jacksonbelanger <jacksonlbelanger@gmail.com>
2024-10-29 20:12:01 -0400

    rework file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+import Login from "./pages/Login/Login";
+import SideBar from "./components/SideBar/SideBar";
+import MainContent from "./components/MainContent/MainContent";
 
 export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  );
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return(
+      (loggedIn ?
+          <Login />
+      :
+      <div className="app-body">
+          <SideBar />
+          <MainContent />
+      </div>
+      )
+  )
 }
